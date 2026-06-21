ed52d18627d8242104b06d1a786648ab247d6b80
Sathish-SB-Dev <sathishsrini541@gmail.com>
2026-06-21 23:17:37 +0530

    domain registered v1

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
 	// BASE_URL: 'http://localhost:5000',
-	BASE_URL: 'https://l-torks-controls-api-production.up.railway.app',
+	BASE_URL: 'https://api.l-torks-controls.online/',
+	// BASE_URL: 'https://l-torks-controls-api-production.up.railway.app',
 	API_VERSION: 'v1',
 	TENANT_ID: '1',
 }
