c8ac5f06e5e51e4779762246add90ea6136a7ff1
Sathish-SB-Dev <sathishsrini541@gmail.com>
2026-07-12 23:33:46 +0530

    fix: update assets/js/config.js with production API domain

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-	BASE_URL: 'http://localhost:5000',
-	// BASE_URL: 'https://api.l-torks-controls.online',
+	// BASE_URL: 'http://localhost:5000',
+	BASE_URL: 'https://api.ltorkcontrols.com',
 	// BASE_URL: 'https://l-torks-controls-api-production.up.railway.app',
 	API_VERSION: 'v1',
 	TENANT_ID: '1',
 }
 
-const SITE_URL = 'http://localhost:5500'
+const SITE_URL = 'https://stage.ltorkcontrols.com'
