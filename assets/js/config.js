f8a010e4930bedd5e8826879146b77f6c8acdd9b
Sathish-SB-Dev <sathishsrini541@gmail.com>
2026-07-13 00:26:11 +0530

    fix: stage config - BASE_URL to stage.api.ltorkcontrols.com + CSP update

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
 	// BASE_URL: 'http://localhost:5000',
-	BASE_URL: 'https://api.ltorkcontrols.com',
+	// BASE_URL: 'https://api.ltorkcontrols.com',
+	BASE_URL: 'https://stage.api.ltorkcontrols.com',
 	// BASE_URL: 'https://l-torks-controls-api-production.up.railway.app',
 	API_VERSION: 'v1',
 	TENANT_ID: '1',
