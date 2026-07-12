4f30c930ade4e8d1dee109f673b3d8d2411065ae
Sathish-SB-Dev <sathishsrini541@gmail.com>
2026-07-12 23:18:39 +0530

    fix: update API domain to api.ltorkcontrols.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 	// BASE_URL: 'http://localhost:5000',
-	BASE_URL: 'https://api.l-torks-controls.online',
+	BASE_URL: 'https://api.ltorkcontrols.com',
 	// BASE_URL: 'https://l-torks-controls-api-production.up.railway.app',
 	API_VERSION: 'v1',
 	TENANT_ID: '1',
